6316725b1afbc47395b9c501d19418f036fe9aeb
mlittle98 <111534292+mlittle98@users.noreply.github.com>
2022-11-07 21:05:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// I used this function from your story card!
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
